26da470a30d5e03c0385327ebdd5626524b94fac
Pierre Philippon <pierre@agile-enabler.com>
2021-06-30 10:27:08 +0200

    feat(timeline): implement vertical scroll

--- src/App.js
+++ src/App.js
@@ -3,7 +3,35 @@ import Timeline from "./components/Timeline/Timeline";
 function App() {
   return (
     <div className="App">
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
       <Timeline />
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
     </div>
   );
 }
--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -1,12 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import TimelineItem from "../TimelineItem/TimelineItem";
 
 import "./timeline.css";
 
 export default function Timeline() {
+  const [scroll, setScroll] = useState(0);
+
+  const onScroll = () => {
+    const windowScroll = document.documentElement.scrollTop;
+
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+
+    // https://stackoverflow.com/questions/22675126/what-is-offsetheight-clientheight-scrollheight
+
+    const scrolled = (windowScroll / height) * 100;
+    setScroll(scrolled);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
   return (
     <div className="timeline-container">
+      <div className="timeline-line_container">
+        <div
+          className="timeline-line_fill"
+          style={{ height: `${scroll}%` }}
+        ></div>
+      </div>
       <TimelineItem />
       <TimelineItem />
       <TimelineItem />
